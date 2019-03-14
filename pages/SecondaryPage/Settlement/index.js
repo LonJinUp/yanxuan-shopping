f78a531556cfd58ab7d82da07d51ac53f4cda8d8
LonJin <LonJin_up@163.com>
2019-03-14 16:44:59 +0800

    备备份

---
+++ pages/SecondaryPage/Settlement/index.js
@@ -0,0 +1,85 @@
+// pages/SecondaryPage/Settlement/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //优惠券文字提示,控制颜色
+    coupon:true,
+    // 判断有没有地址
+    Address:true
+  },
+  //添加地址
+  Address:function(e){
+    wx.chooseAddress({
+      success(res) {
+        console.log(res.userName)
+        console.log(res.postalCode)
+        console.log(res.provinceName)
+        console.log(res.cityName)
+        console.log(res.countyName)
+        console.log(res.detailInfo)
+        console.log(res.nationalCode)
+        console.log(res.telNumber)
+      }
+    })
+  },
+
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/SecondaryPage/product/index.js
+++ pages/SecondaryPage/product/index.js
@@ -10,6 +10,10 @@ Page({
     // 查看更多；
     MoreNum:"3",
     MoreText:"更多参数",
+    //点击弹出购买选择
+    ShowShare: false,
+    //初始购买数量
+    BuyNumber:"1",
     //模拟数据
    list:{
     "name": "商品介绍",
@@ -42,6 +46,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
 
+
+  // 更多参数
   more:function(e){
     var NowMore=e.target.dataset.more;
     var that = this;
@@ -57,6 +63,40 @@ Page({
       })
     }
    
+  },
+  // 尺码选择
+  //点击关闭
+  HideShare: function (e) {
+    var that = this;
+    that.setData({
+      ShowShare: false
+    })
+  },
+  ShowShare: function (e) {
+    console.log(e);
+    var that = this;
+    that.setData({
+      ShowShare: true,
+    })
+  },
+  //减
+  minus: function (e) {
+   if(this.data.BuyNumber>1){
+     let num = parseInt(this.data.BuyNumber) - 1;
+     var that = this;
+     that.setData({
+       BuyNumber:num
+     })
+   }
+
+  },
+  //加
+  add: function (e) {
+    let num =parseInt(this.data.BuyNumber)+1;
+    var that=this;
+    that.setData({
+      BuyNumber:num++
+    })
   },
   onLoad: function (e) {
    
